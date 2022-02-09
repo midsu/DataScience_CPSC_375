dc4a41cb4ab80e51e521a65a2c60281cdf0141ba
Hamid Suha <34997278+midsu@users.noreply.github.com>
2022-02-08 19:01:50 -0800

    Update classWork_3.R

--- classWork_3.R
+++ classWork_3.R
@@ -1,22 +1,6 @@
 library(ggplot2)
+
 summary(iris)
 
-ggplot(data=iris)+geom_boxplot(mapping = aes(x=Petal.Length), binwidth=0.2, color="orange")
-
-x <- c(8,11,17,36,25,24,1,20,64)
-
-summary(x)
-
-sort(x)
-
-quantile(x)
-
-IQR(x)
-#IQR = 75% - 25% -> 14
-
-ggplot(data=iris)+geom_boxplot(mapping = aes(y=Petal.Length))
-
-x <- c(8, 11, 17, 36, 25, 24, 1, 20, 64)
-mydata <- data.frame(col1=x)
-ggplot(data = mydata) + geom_boxplot(mapping = aes(y=col1))
+ggplot(data=iris)+geom_histogram(mapping = aes(x=Petal.Length), binwidth=0.2, color="orange")
 
